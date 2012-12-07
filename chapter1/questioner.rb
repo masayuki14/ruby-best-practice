9cf4f41d9ec20d99221b0862463510879a0524e2
Masayuki <masayuki144@gmail.com>
2012-12-07 10:13:36 +0900

    Use StringIO, ver.2

--- chapter1/questioner.rb
+++ chapter1/questioner.rb
@@ -5,22 +5,17 @@ class Questioner
     @output = output
   end
 
-  def inquire_about_happiness
-    ask("Are you happy?") ? "Good I'm Glad." : "That's so Bad."
-  end
-
   def ask(question)
     @output.puts question
-    response = yes_or_no(@input.gets.chomp)
-    response.nil? ? ask(question): response
-  end
-
-  def yes_or_no(response)
+    response = @input.gets.chomp
     case(response)
     when /^y(es)?$/i
       true
     when /^no?$/i
       false
+    else
+      @output.puts "I don't understand."
+      ask question
     end
   end
 
--- chapter1/test_qusetioner.rb
+++ chapter1/test_qusetioner.rb
@@ -13,46 +13,28 @@ class QusetionerTest < Test::Unit::TestCase
     @question   = "Are you happy?"
   end
 
-  %w[y Y YES yes].each do |yes|
-    must "return true when yes_or_no parses #{yes}" do
-      assert @questioner.yes_or_no(yes), "#{yes.inspect} expected to parse as true"
-    end
-  end
-
-  %w[y Y YES yes].each do |yes|
+  %w[y Y YES yes Yes yeS].each do |yes|
     must "return true when parsing #{yes}" do
       provide_input(yes)
-      assert @questioner.ask(@question), "#{yes.inspect} expected to parse as true"
+      assert @questioner.ask(@question), "#{yes.inspect} expected to be true"
+      expect_output(@question)
     end
   end
 
-  %w[n no NO nO].each do |no|
-    must "return false when yes_or_no parses #{no}" do
-      assert !@questioner.yes_or_no(no), "#{no.inspect} expected to parse as false"
-    end
-  end
-
-  %w[n no NO nO].each do |no|
+  %w[n no NO nO N].each do |no|
     must "return false when parsing #{no}" do
       provide_input(no)
-      assert !@questioner.ask(@question), "#{no.inspect} expected to parse as false"
+      assert !@questioner.ask(@question), "#{no.inspect} expected to be false"
+      expect_output(@question)
     end
   end
 
-  %w[Note Yesterday wxyz].each do |mu|
-    must "return nil because #{mu} is not avariant of 'yes' or 'no'" do
-      assert_nil @questioner.yes_or_no(mu), "#{mu.inspect} expected to parse as nil"
+  [['y', true],['n', false]].each do |input,state|
+    must "continue to ask for input until given #{input}" do
+      provide_input("Note\nYesterday\nwxyz\n#{input}")
+      assert_equal state, @questioner.ask(@question)
+      expect_output("#{@question}\nI don't understand.\n"*3 + "#{@question}\n")
     end
-  end
-
-  must "respond 'Good I'm Glad' whe inquire_about_happiness gets 'yes'" do
-    provide_input('yes')
-    assert_equal "Good I'm Glad.", @questioner.inquire_about_happiness
-  end
-
-  must "respond 'That's so Bad.' when inquire_about_happiness gets 'no'" do
-    provide_input('no')
-    assert_equal "That's so Bad.", @questioner.inquire_about_happiness
   end
 
   def provide_input(string)
@@ -62,6 +44,6 @@ class QusetionerTest < Test::Unit::TestCase
 
 
   def expect_output(string)
-    assert_equal string, @output.string
+    assert_equal string.chomp, @output.string.chomp
   end
 end
